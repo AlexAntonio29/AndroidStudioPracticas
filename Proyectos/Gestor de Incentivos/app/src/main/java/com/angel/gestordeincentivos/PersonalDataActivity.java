cf675532602fed1d7998cb214800c20ba3a61721
AlexAntonio29 <alexantonioangel29@gmail.com>
2025-01-08 10:33:53 -0600

    update PersonalDataGestorIncentivos v1.1

--- Proyectos/Gestor de Incentivos/app/src/main/java/com/angel/gestordeincentivos/PersonalDataActivity.java
+++ Proyectos/Gestor de Incentivos/app/src/main/java/com/angel/gestordeincentivos/PersonalDataActivity.java
@@ -21,8 +21,8 @@ public class PersonalDataActivity extends AppCompatActivity {
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_datos_personales);
+
         this.context=this;
         mparent=findViewById(R.id.llContenedorDatosPersonales);
         bGuardar= findViewById(R.id.bGuardarPersonalData);
